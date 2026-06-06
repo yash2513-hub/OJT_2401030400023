dab030ce2ca3583a4576260913c3c88e76a278b4
yash2513-hub <yash2513asodariya@gmail.com>
2026-06-06 23:03:25 +0530

    Add files via upload

---
+++ Day 19/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+      </main>
+      <Body />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
